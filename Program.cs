771423ba36aad2b3981c6d0f9dae7086f8912880
alex-geraschenko <68548043+alex-geraschenko@users.noreply.github.com>
2020-07-22 14:14:32 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
