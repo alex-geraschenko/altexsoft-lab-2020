058685f4df56b0530a488ae23d9cc1840cdbd44b
Oleksandr Gerashchenko <avgera@gmail.com>
2020-09-15 20:52:15 +0300

    Initial task 4 structure

---
+++ task4/HomeTask4.Cmd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeTask3.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
