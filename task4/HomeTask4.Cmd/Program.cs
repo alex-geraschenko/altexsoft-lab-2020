b8e65b8ba3cb5942568cb38e83608e893a600704
Oleksandr Gerashchenko <avgera@gmail.com>
2020-09-16 14:27:52 +0300

    Add project skeleton

--- task4/HomeTask4.Cmd/Program.cs
+++ task4/HomeTask4.Cmd/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace HomeTask3.Cmd
+/// <summary>
+/// A skeleton for the Home Task 4 in AltexSoft Lab 2020
+/// For more details how to organize configuration, logging and dependency injections in console app
+/// watch https://www.youtube.com/watch?v=GAOCe-2nXqc
+///
+/// For more information about General Host
+/// read https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-3.1
+///
+/// For more information about Logging
+/// read https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1
+///
+/// For more information about Dependency Injection
+/// read https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.1
+/// </summary>
+namespace HomeTask4.Cmd
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(config =>
+                {
+                })
+                .ConfigureLogging(config =>
+                {
+                    config.ClearProviders();
+                    config.AddConsole();
+                })
+                .Build();
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Hello World!");
+
             Console.ReadLine();
         }
     }
